31c1fe3cc21c5b733d8e85b1cb5b31abd8081a1d
DSC <sachev.andrei@gmail.com>
2023-12-14 01:50:43 +0600

    Add AI generated test on ENT; Clear code;

--- infomatrix.py
+++ infomatrix.py
@@ -9,6 +9,7 @@ from tempfile import TemporaryFile
 import speech_recognition as sr
 from pydub import AudioSegment
 import re
+import json
 
 import os 
 
@@ -29,56 +30,39 @@ admin_id = ''
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
-	markup = types.ReplyKeyboardMarkup(row_width=2)
-	itembtn1 = types.KeyboardButton(text = 'Course for ENT')
-	itembtn2 = types.KeyboardButton('Course progr')
-	itembtn3 = types.KeyboardButton('Help AI')
-	markup.add(itembtn1, itembtn2, itembtn3)
-	bot.reply_to(message, "Welcome test msg", reply_markup=markup)
+	bot.reply_to(message, '''
+Сәлеметсіз бе, мен информатика бойынша ҰБТ-ға дайындалуға көмектесетін AI-ботпын!
+Сізбен жұмыс істейік!
+Нақты не істегіңіз келетінін таңдаңыз;
 
-@bot.message_handler(commands=['start', 'help'])
-def welcome(message):
-	markup = types.ReplyKeyboardMarkup(row_width=2)
-	itembtn1 = types.KeyboardButton('Course for ENT')
-	itembtn2 = types.KeyboardButton('Course progr')
-	itembtn3 = types.KeyboardButton('Help AI')
-	markup.add(itembtn1, itembtn2, itembtn3)
-	bot.reply_to(message, "Welcome test msg", reply_markup=markup)
+Ботта қол жетімді командалар:
+/start /help - сіз Бұл хабарламаны көресіз және мәзірдің негізгі бетіне өтесіз
+/report - сіз ботты жақсартуға немесе мәселеге өтініш бере аласыз.
+		''', reply_markup=home_menu())
 
 
 @bot.message_handler(func=lambda message: True)
 def response(message):
-	if message.text == "Course for ENT":					# ENT course choise
-		markup = types.ReplyKeyboardMarkup(row_width=2)
-		itembtn1 = types.KeyboardButton('ENT Themes')
-		itembtn2 = types.KeyboardButton('ENT Zadania')
-		itembtn3 = types.KeyboardButton('Sprosi u AI')
-		markup.add(itembtn1, itembtn2, itembtn3)
-		bot.reply_to(message, "Course on ENT", reply_markup=markup)
+	if message.text == "Курстар ҰБТ":
+		bot.reply_to(message, "Course on ENT", reply_markup=menu(2,
+			'Тақырыптың ҰБТ'
+			'Тапсырмалар ҰБТ'
+			'AI сұраңыз' ))
 
-	elif message.text == "Course progr":
-		markup = types.ReplyKeyboardMarkup(row_width=2)
-		itembtn1 = types.KeyboardButton('Theme 1')
-		itembtn2 = types.KeyboardButton('Theme 2')
-		itembtn3 = types.KeyboardButton('Theme 3')
-		itembtn4 = types.KeyboardButton('Theme 4')
-		markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
-		msg_bot = bot.send_message(message.chat.id,"Course prog\n Choose Theme", reply_markup=markup)
+	elif message.text == "Бағдарламалау курстары":
+		msg_bot = bot.send_message(message.chat.id,"Course prog\n Choose Theme", reply_markup=menu(1,
+			'Theme 1'
+			'Theme 2'
+			'Theme 3'
+			'Theme 4'))
 		bot.register_next_step_handler(msg_bot,)
 
-	elif message.text == "ENT Themes":
-		markup = types.ReplyKeyboardMarkup(row_width=1)
-		theme_btn_1 = types.KeyboardButton('1.ENT. Системы исчисления в информатике: перевод чисел')
-		theme_btn_2 = types.KeyboardButton('2.ENT. Хранение данных и Память в Информатике')
-		theme_btn_3 = types.KeyboardButton('3.ENT. Сети и Их Топологии в Информатике')
-		theme_btn_4 = types.KeyboardButton('4.ENT. Основы Баз Данных и Реляционные Системы Управления Базами Данных (СУБД)')
-
-		markup.add( theme_btn_1,
-					theme_btn_2,
-					theme_btn_3,
-					theme_btn_4 )
-
-		msg_bot = bot.send_message(message.chat.id,"Please choose theme", reply_markup=markup)
+	elif message.text == "Тақырыптың ҰБТ":
+		msg_bot = bot.send_message(message.chat.id,"Please choose theme", reply_markup=menu(1,
+			'1.ENT. Системы исчисления в информатике: перевод чисел',
+			'2.ENT. Хранение данных и Память в Информатике',
+			'3.ENT. Сети и Их Топологии в Информатике',
+			'4.ENT. Основы Баз Данных и Реляционные Системы Управления Базами Данных (СУБД)'))
 
 	elif re.fullmatch(r'\d\.\w+\..+' , message.text):
 		spl_txt = message.text.split(".")
@@ -95,35 +79,32 @@ def response(message):
 			text += line
 		file.close()
 
-		markup = types.ReplyKeyboardMarkup(row_width=2)
-		back_btn = types.KeyboardButton('Back')
-		ai_btn = types.KeyboardButton('Sprosi u AI')
-		quest_btn = types.KeyboardButton('Quest')
+		msg_bot = bot.send_message(message.chat.id, text , reply_markup = menu(2,
+																	'Тапсырма',
+																	'AI сұраңыз',
+																	'Артқа'))
 
-		markup.add(	ai_btn,
-					quest_btn,
-					back_btn )
+		def func(message):
+			nonlocal spl_txt
+			theme_kb(message, spl_txt[2])
 
-		msg_bot = bot.send_message(message.chat.id, text , reply_markup = markup)
+		bot.register_next_step_handler(msg_bot, func)
 
-	elif message.text == "Back":
-		markup = types.ReplyKeyboardMarkup(row_width=2)
-		itembtn1 = types.KeyboardButton('Course for ENT')
-		itembtn2 = types.KeyboardButton('Course progr')
-		itembtn3 = types.KeyboardButton('Help AI')
-		markup.add(itembtn1, itembtn2, itembtn3)
+	elif message.text == "Артқа":
+		msg_bot = bot.send_message(message.chat.id , "Home" , reply_markup = home_menu())
 
-		msg_bot = bot.send_message(message.chat.id , "Home" , reply_markup = markup)
+	elif message.text == "Тапсырмалар ҰБТ":
+		msg_bot = bot.send_message(message.chat.id,"Please choose theme", reply_markup=menu(1, 
+			'1.Задания ЕНТ. Санды өлшемдері және оларды басқару жолдары'
+			'2.Задания ЕНТ. Хранение данных и Память',
+			'3.Задания ЕНТ. Сети и Их Топологии', 
+			'4.Задания ЕНТ. Основы Баз Данных и Реляционные Системы Управления Базами Данных (СУБД)'))
 
-	elif message.text == "ENT Zadania":
+	elif message.text == "Көмек AI":
 		msg_bot = bot.send_message(message.chat.id,"Please write question")
 		bot.register_next_step_handler(msg_bot,help_ai)
 
-	elif message.text == "Help AI":
-		msg_bot = bot.send_message(message.chat.id,"Please write question")
-		bot.register_next_step_handler(msg_bot,help_ai)
-
-	elif message.text == "Sprosi u AI":
+	elif message.text == "AI сұраңыз":
 		msg_bot = bot.send_message(message.chat.id,"Please write question")
 		bot.register_next_step_handler(msg_bot,help_ai)
 
@@ -131,18 +112,93 @@ def response(message):
 		bot.reply_to(message, "I dont know")
 
 		
-def help_ai_on_themes(message , material):
-	prompt_with_material = f"{prompt}\nMaterial: {material}\nAnswer:"
-	response = openai.Completion.create(
-		engine="text-davinci-003",
-		prompt=prompt_with_material,
-		max_tokens=150,
-		temperature=0.7,
-		stop=stop
-	)
+# def help_ai_on_themes(message , material):
+# 	prompt_with_material = f"{prompt}\nMaterial: {material}\nAnswer:"
+# 	response = openai.Completion.create(
+# 		engine="text-davinci-003",
+# 		prompt=prompt_with_material,
+# 		max_tokens=150,
+# 		temperature=0.7,
+# 		stop=stop
+# 	)
 
-	answer = response.choices[0].text.strip()
-	return answer
+# 	answer = response.choices[0].text.strip()
+# 	return answer
+
+def theme_kb(message, theme):
+	if message.text == "Артқа":
+		bot.send_message(message.chat.id,"Басты бет" ,markup = home_menu())
+	elif message.text == "AI сұраңыз":
+		help_ai(message)
+	elif message.text == "Тапсырма":
+		q_test(message, theme)
+
+
+def q_test(message, theme):
+	text = f'''
+Сгенерируй мне тест по теме {theme}, но выйдай его в определенном виде для парсинга: 
+создай json объект вне массива в котором есть массив test, 
+в котором есть 6 вопросов-объектов сотоящий из полей quest- сам текст вопроса, массив ответов answers- массив вариантов ответов состоящий из полей code и text;
+и еще поле true_ans которое должно быть в объекте вопроса и содержать код правельного ответа, а не в объекте ответа;
+НЕ ПИШИ НИЧЕГО КРОМЕ JSON ФАЙЛА.'''
+
+	print(text)
+
+	user_score = 0
+
+	try:
+		completion = client.chat.completions.create(
+			model="gpt-3.5-turbo",
+			messages=[
+				{"role": "system", "content": "You are a self-improvement assistant."},
+				{"role": "user", "content": f"{text}"}
+			]
+		)
+		response_message = completion.choices[0].message.content  # Accessing the content directly
+		print(response_message)  # For debugging
+	except Exception as e:
+		bot.reply_to(message, f"An error occurred: {str(e)}")
+
+	quest = json.loads(response_message)
+
+	def func(msg, ans):
+		nonlocal quest
+		nonlocal user_score
+
+		if ans == len(quest["test"]):
+			if quest["test"][ans-1]["true_ans"] == msg.text.split(".")[0]:
+				bot.send_message(msg.chat.id, "Молодец!")
+				user_score+=1
+			else:
+				bot.send_message(msg.chat.id, "В следущий раз получиться!")
+
+			bot.send_message(msg.chat.id, f'Вы закончили тест! Ваш результат {user_score}/{(len(quest["test"]))}',
+				reply_markup = menu(1,"Артқа"))
+			return
+		elif ans > 0:
+			if quest["test"][ans-1]["true_ans"] == msg.text.split(".")[0]:
+				bot.send_message(msg.chat.id, "Молодец!")
+				user_score+=1
+			else:
+				bot.send_message(msg.chat.id, "В следущий раз получиться!")
+
+		print(quest["test"][ans-1]["true_ans"])
+		print(msg.text.split(".")[0])
+
+		answers = []
+		for qst in quest["test"][ans]["answers"]:
+			answers.append(f'{qst["code"]}. {qst["text"]}')
+
+		msg_bot = bot.send_message(msg.chat.id, 
+						f'{ans+1}. {quest["test"][ans]["quest"]}',
+						reply_markup = menu(2, *answers))
+
+		bot.register_next_step_handler(msg_bot, lambda msg: func(msg, ans+1))
+
+	func(message, 0)
+
+
+
 
 
 def help_ai(message):
@@ -170,8 +226,21 @@ def report(msg):
 
 	report_text = f"New report from user {user_id} in chat {chat_id}:\n\n{text}"
 	bot.send_message(admin_id, report_text)
-	bot.send_message(chat_id, "Thank you for your report. We will review it as soon as possible.")
+	bot.send_message(chat_id, "Есеп бергеніңіз үшін рахмет. Біз оны мүмкіндігінше тезірек қарастырамыз.")
 
+
+def home_menu():
+	return menu(2, 'Тақырыптың ҰБТ' , 'Тапсырмалар ҰБТ' , 'AI сұраңыз')
+
+def menu(rows, *text_buttons):
+	markup = types.ReplyKeyboardMarkup(row_width=rows)
+	btns = []
+
+	for btn in text_buttons:
+		btns.append(types.KeyboardButton(btn))
+
+	markup.add(*btns)
+	return markup
 
 
 #tts and stt
